ed75d3b4b943fe5a9106e4824c9a86e3daa20a66
Sem van Elteren <1062594@hr.nl>
2023-10-19 16:18:08 +0200

    more details at detail page

--- resources/views/workouts/show.blade.php
+++ resources/views/workouts/show.blade.php
@@ -10,6 +10,19 @@
 
                     <div class="card-body">
                         <p>{{ $workout->description }}</p>
+                        <p>Sets: {{ $workout->sets }}</p>
+                        <p>Reps: {{ $workout->reps }}</p>
+                        <p>Weight: {{ $workout->weight }}</p>
+                        <!-- edit button -->
+                        <a href="{{ route('workouts.edit', $workout->id) }}" class="btn btn-primary">Edit</a>
+                        <!-- delete button -->
+                        <form action="{{ route('workouts.destroy', $workout->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+
+                        <!-- return to index -->
                         <a href="{{ route('workouts.index') }}" class="btn btn-primary">Back</a>
                     </div>
                 </div>
